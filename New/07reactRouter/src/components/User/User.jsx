7d459cf1469ade9440d54f142220462c22fb3f62
Lucky Verma <luckykishoreverma0404@gmail.com>
2025-08-21 09:03:18 +0530

    Remove New folder from repository

--- New/07reactRouter/src/components/User/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-function User(){
-    const {userid}=useParams()
-    return (
-        <div className='bg-Gray-600 text-white text-3xl p-4'>User:{userid}</div>
-    )
-}
-export default User
